578f48f2c6d4484db246ed240b8f25540a6087ff
Stef McCuistion <stef.mccuistion@gmail.com>
2026-05-11 21:15:51 -0500

    Added header label to layout

--- MayaRigControlCurvesAutomater.py
+++ MayaRigControlCurvesAutomater.py
@@ -16,3 +16,15 @@ class Window(QtWidgets.QDialog):
         super().__init__(parent=get_maya_main_win())
         self.setWindowTitle("Control Curves Automater")
         self.resize(300, 400)
+        self._mk_main_layout()
+
+    def _mk_main_layout(self):
+        self._layout = QtWidgets.QVBoxLayout(self)
+        # make UI
+        # header label
+        self._header_label = QtWidgets.QLabel("MR Control Curves Automater",
+                                              self)
+        self._header_label.setStyleSheet("font-size: 20px; "
+                                         "font-weight: bold;")
+        self._header_label.setAlignment(QtCore.Qt.AlignCenter)
+        self._layout.addWidget(self._header_label)
